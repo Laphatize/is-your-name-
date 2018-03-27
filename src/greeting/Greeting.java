cf5012d47268da8e181e229110c953787e89a054
Pranav Ramesh <laphatize.me@gmail.com>
2018-03-27 12:59:45 -0400

    Add files via upload

---
+++ src/greeting/Greeting.java
@@ -0,0 +1,23 @@
+package greeting;
+
+public class Greeting {
+
+		public static void main(String[] args) {
+		
+		
+		String name = "bob";
+		System.out.println("Hello " + name + "!");
+		
+		
+		
+		// Check to see if the name is my name or someone else's.
+		if (name == "Pranav") {
+			
+			System.out.println("Confirmed.");
+			
+		} else {
+			System.out.println("Nope");
+	 
+			};
+				};
+					};
